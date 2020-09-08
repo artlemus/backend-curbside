28c83c109e3597cd1848772fefd07e0b586b9e38
artlemus <artlemus91@gmail.com>
2020-09-08 11:18:38 -0700

    cors headers updated on serverjs

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const connectDB = require('./config/db')
 const app = express()
 
 connectDB()
-
 app.use(express.json({ extended: false }))
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With, Content-Type, Accept, Authorization')
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+    return res.status(200).json({})
+  }
+  next()
+})
 
 app.get('/', (req, res) => {
   res.send('API Running')
